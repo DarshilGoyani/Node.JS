94ea45b3744352333c61777549da6ce8eb333518
DarshilGoyani <darshilgoyani05@gmail.com>
2025-12-01 12:47:07 +0530

    custom server

--- ch-5 Custom Server/server.js
+++ ch-5 Custom Server/server.js
@@ -1,8 +1,48 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res) => {
-    res.write("<h1>Server is live</h1>");
-    res.end();
+    if (req.url === '/favicon.ico') {
+        return;
+    }
+
+    const msg = `New USer ${new Date()} IP Adress := ${req.socket.remoteAddress}\n`
+
+    fs.appendFile('userLog.txt',msg, (err) => {})
+    console.log(req.url);
+
+    let fileName = "";
+
+    switch (req.url) {
+        case '/':
+            fileName = "index.html"
+            break;
+
+        case '/contact':
+            fileName = "contact.html"
+            break;
+
+        case '/blog':
+            fileName = "blog.html"
+            break;
+
+        case '/gallary':
+            fileName = "gallary.html"
+            break;
+    
+        default:
+            fileName = "404.html"
+            break;
+    }
+
+    fs.readFile(fileName,(err,result) => {
+        if (err) {
+            console.log(err);
+            
+        }
+        res.end(result);
+    })
+
 })
 
 server.listen(8000,(err) =>{
